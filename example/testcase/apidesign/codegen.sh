96721b01e8a3a1816c9345e8cb073d07d77757b0
paleviews <lyatqh@hotmail.com>
2024-11-19 20:21:27 +0800

    chore(examples): rename swagger files

--- example/testcase/apidesign/codegen.sh
+++ example/testcase/apidesign/codegen.sh
@@ -5,5 +5,5 @@ rm -rf golang/*
 find proto -name "*.proto" | xargs \
   protoc --proto_path proto --proto_path ../../../descriptor/proto \
     --hapi_opt paths=source_relative \
-    --hapi_opt doc_file=doc/doc.yaml \
+    --hapi_opt doc_file=doc/swagger.yaml \
     --hapi_out golang
